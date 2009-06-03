d69c389c28e949a9925a3d7fb4b87778827bdab8
hudvin <hudvin@70bebeea-5060-11de-81c9-c396dc251947>
2009-06-03 20:18:24 +0000

    добавлена обработка отсутствия карты при старте

--- src/com/nevilon/bigplanet/core/Log.java
+++ src/com/nevilon/bigplanet/core/Log.java
@@ -12,6 +12,7 @@ public class Log {
 	private static Logger logger = Logger.getLogger("MAIN");
 	
 	static{
+		/*
 		try {
 			logger.setLevel(Level.ALL);
 			FileHandler fileHandler = new FileHandler("/sdcard/bigplanet/log.txt");
@@ -23,6 +24,7 @@ public class Log {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		*/
 	}
 	
 	public static void message(String tag,String msg){
