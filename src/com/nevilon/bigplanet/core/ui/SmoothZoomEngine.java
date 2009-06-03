46f05b333d7071eaeb5b7ec12a5e3597c8b25a25
hudvin <hudvin@70bebeea-5060-11de-81c9-c396dc251947>
2009-06-03 20:40:11 +0000

    исправлены баги с провайдерами

--- src/com/nevilon/bigplanet/core/ui/SmoothZoomEngine.java
+++ src/com/nevilon/bigplanet/core/ui/SmoothZoomEngine.java
@@ -1,94 +1,115 @@
 package com.nevilon.bigplanet.core.ui;
 
 import java.util.LinkedList;
+import java.util.concurrent.Semaphore;
 
 import com.nevilon.bigplanet.core.AbstractCommand;
 import com.nevilon.bigplanet.core.PhysicMap;
-import com.nevilon.bigplanet.core.Utils;
 
 public class SmoothZoomEngine {
 
-	
 	private static SmoothZoomEngine sze;
 	
 	private LinkedList<Integer> scaleQueue = new LinkedList<Integer>();
 
 	private Float scaleFactor = 1000f;
-	
-	
+
 	private AbstractCommand updateScreen;
-	
+
 	private AbstractCommand reloadMap;
-	
-	public static SmoothZoomEngine getInstance(){
-		if(sze==null){
+
+	public static SmoothZoomEngine getInstance() {
+		
+			
+		
+		if (sze == null) {
 			sze = new SmoothZoomEngine();
 		}
 		return sze;
 	}
-	
-	public void setUpdateScreenCommand(final AbstractCommand updateScreen){
+
+	public void setUpdateScreenCommand(final AbstractCommand updateScreen) {
 		this.updateScreen = updateScreen;
 	}
-	
-	public void setReloadMapCommand(final AbstractCommand reloadMap){
+
+	public void setReloadMapCommand(final AbstractCommand reloadMap) {
 		this.reloadMap = reloadMap;
 	}
-	
-	public void nullScaleFactor(){
+
+	public void nullScaleFactor() {
 		synchronized (scaleFactor) {
 			scaleFactor = 1000f;
-		} 
+		}
 	}
-	
-	private  SmoothZoomEngine() {
+
+	private SmoothZoomEngine() {
+		System.out.println("create queue");
+		createQueue();
+	}
+
+	private void createQueue() {
 		new Thread() {
 
 			@Override
 			public void run() {
 				boolean isEmpty = true;
-				if(updateScreen!=null){
+				if (updateScreen != null) {
 					updateScreen.execute();
-						
+
 				}
 				double endScaleFactor;
-				int PAUSE = 10;
+				int PAUSE = 15;
 				while (true) {
 					if (scaleQueue.size() > 0) {
 						isEmpty = false;
 						int scaleDirection = scaleQueue.removeFirst();
 						endScaleFactor = scaleDirection == -1 ? scaleFactor / 2
 								: scaleFactor * 2;
-						int z  = PhysicMap.zoom ;
-						System.out.println("zoom " + PhysicMap.zoom+" scaleFactor "  + Utils.getZoomLevel(scaleFactor/1000));
-						if((scaleDirection==-1 && z<16) || (scaleDirection==1 && z>0)){
-							if(!(endScaleFactor > 8000 || endScaleFactor < 125)){
-								synchronized (scaleFactor) {
-
+						int z = PhysicMap.getZoomLevel();
+						if ((scaleDirection == -1 && z < 16)
+								|| (scaleDirection == 1 && z > 0)) {
+							if (!(endScaleFactor > 8000 || endScaleFactor < 125)) {
+								System.out.println("smooth scaling");
+								synchronized (sze) {
+									// synchronized (scaleFactor) {
 									do {
 										try {
 											Thread.sleep(PAUSE);
 											scaleFactor = scaleFactor
 													+ (scaleDirection) * 25;
 											// обновить экран
-							
+
 											updateScreen.execute(new Float(
 													scaleFactor / 1000));
 										} catch (InterruptedException e) {
 											e.printStackTrace();
 										}
-									} while (!(scaleFactor == (endScaleFactor)));	
+									} while (!(scaleFactor == (endScaleFactor)));
 								}
 							}
-
-						} if (!isEmpty && scaleQueue.size()==0) {
-							System.out.println("reload");
-							isEmpty = true;
-							reloadMap.execute(new Float(scaleFactor / 1000));
-						}
+							// }
 
 						}
-				//	}
+						
+
+							if (!isEmpty && scaleQueue.size() == 0) {
+								System.out.println("reload");
+								isEmpty = true;
+								try {
+									Thread.sleep(100);
+									reloadMap.execute(new Float(
+											scaleFactor / 1000));
+									//semaphore.release();
+								} catch (InterruptedException e) {
+									// TODO Auto-generated catch block
+									e.printStackTrace();
+								}
+							
+						}
+
+					}
+					// }
+
 				}
 			}
 
@@ -96,7 +117,10 @@ public class SmoothZoomEngine {
 	}
 
 	public void addToScaleQ(int direction) {
-		scaleQueue.addLast(direction);
+		synchronized (scaleQueue) {
+			System.out.println("add to scale " + direction);
+			scaleQueue.addLast(direction);
+		}
 	}
 
 }
