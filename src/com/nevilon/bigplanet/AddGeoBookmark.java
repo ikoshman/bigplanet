df596e2f107b0dcfced9a5083f18ef9ce95ee56d
hudvin <hudvin@70bebeea-5060-11de-81c9-c396dc251947>
2009-06-03 19:34:47 +0000

    добавлен прототип диалога для добавления геозакладки

---
+++ src/com/nevilon/bigplanet/AddGeoBookmark.java
@@ -0,0 +1,14 @@
+package com.nevilon.bigplanet;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddGeoBookmark extends Activity {
+
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.addgeobookmark);
+	}
+}
--- src/com/nevilon/bigplanet/R.java
+++ src/com/nevilon/bigplanet/R.java
@@ -16,15 +16,20 @@ public final class R {
         public static final int ts=0x7f020002;
     }
     public static final class id {
-        public static final int cancelButton=0x7f050004;
-        public static final int downloadInfo=0x7f050002;
-        public static final int radiusLabel=0x7f050001;
-        public static final int radiusSeekbar=0x7f050000;
-        public static final int startButton=0x7f050003;
+        public static final int cancelButton=0x7f050008;
+        public static final int descriptionValue=0x7f050003;
+        public static final int descriptionkLabel=0x7f050002;
+        public static final int downloadInfo=0x7f050006;
+        public static final int nameLabel=0x7f050000;
+        public static final int nameValue=0x7f050001;
+        public static final int radiusLabel=0x7f050005;
+        public static final int radiusSeekbar=0x7f050004;
+        public static final int startButton=0x7f050007;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
-        public static final int savemap=0x7f030001;
+        public static final int addgeobookmark=0x7f030000;
+        public static final int main=0x7f030001;
+        public static final int savemap=0x7f030002;
     }
     public static final class string {
         public static final int GOOGLE_SATELLITE=0x7f040004;
