dac983795ab8402ab79ba15aa07f41e90adea06a
hudvin <hudvin@70bebeea-5060-11de-81c9-c396dc251947>
2009-06-03 19:35:18 +0000

    добавлен запуск активити для добавлния геозакладки по лонгтачу

---
+++ src/com/nevilon/bigplanet/core/ui/OnMapLongClickListener.java
@@ -0,0 +1,9 @@
+package com.nevilon.bigplanet.core.ui;
+
+import android.view.MotionEvent;
+
+public abstract class OnMapLongClickListener {
+	
+	public abstract void onMapLongClick(MotionEvent event);
+
+}
