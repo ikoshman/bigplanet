005257ac547d4c4919c925a69d685bb1857a7272
hudvin <hudvin@70bebeea-5060-11de-81c9-c396dc251947>
2009-06-03 20:12:30 +0000

    добавлено логирование

---
+++ src/com/nevilon/bigplanet/core/Log.java
@@ -0,0 +1,35 @@
+package com.nevilon.bigplanet.core;
+
+import java.io.IOException;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public class Log {
+
+	private static Logger logger = Logger.getLogger("MAIN");
+	
+	static{
+		try {
+			logger.setLevel(Level.ALL);
+			FileHandler fileHandler = new FileHandler("/sdcard/bigplanet/log.txt");
+			fileHandler.setFormatter(new SimpleFormatter());
+			logger.addHandler(fileHandler);
+			 ConsoleHandler conHdlr = new ConsoleHandler();
+			 conHdlr.setFormatter(new SimpleFormatter());
+			 logger.addHandler(conHdlr);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void message(String tag,String msg){
+		String message = tag+":"+ msg; 
+		System.out.println(message	);
+		logger.info(message);
+	}
+	
+		
+}
