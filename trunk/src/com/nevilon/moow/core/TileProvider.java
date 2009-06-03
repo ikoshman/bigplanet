03b2817ecc3e95895b167c2af8c2b0f2bad19671
hudvin <hudvin@70bebeea-5060-11de-81c9-c396dc251947>
2009-06-03 18:34:55 +0000

    поправлен баг со скалированием

--- trunk/src/com/nevilon/moow/core/TileProvider.java
+++ trunk/src/com/nevilon/moow/core/TileProvider.java
@@ -130,63 +130,60 @@ public class TileProvider implements Runnable {
 		}
 
 		private Bitmap findTile(int x, int y, int z) {
-			Bitmap bitmap = null;
-			int offsetX;
-			int offsetY;
-			int offsetParentX;
-			int offsetParentY;
-			int parentTileX;
-			int parentTileY;
-			// получение отступа от начала координат на текущев уровне
-			offsetX = x * 256; // отступ от начала координат по ox
-			offsetY = y * 256; // отступ от начала координат по oy
-			int tmpZ = z;
-			while (bitmap == null && tmpZ <= 17) {
-				tmpZ++;
+            Bitmap bitmap = null;
+            int offsetX;
+            int offsetY;
+            int offsetParentX;
+            int offsetParentY;
+            int parentTileX;
+            int parentTileY;
+            // получение отступа от начала координат на текущев уровне
+            offsetX = x * 256; // отступ от начала координат по ox
+            offsetY = y * 256; // отступ от начала координат по oy
+            int tmpZ = z;
+            while (bitmap == null && tmpZ <= 17) {
+                    tmpZ++;
 
-				int scale = tmpZ - z;
+                    // получение отступа от начала координат на предыдущем уровне
+                    offsetParentX = (int) (offsetX / Math.pow(2, tmpZ - z));
+                    offsetParentY = (int) (offsetY / Math.pow(2, tmpZ - z));
 
-				// получение отступа от начала координат на предыдущем уровне
-				offsetParentX = (int) (offsetX / Math.pow(2, scale));
-				offsetParentY = (int) (offsetY / Math.pow(2, scale));
+                    // получение координат тайла на предыдущем уровне
+                    parentTileX = offsetParentX / 256;
+                    parentTileY = offsetParentY / 256;
 
-				// получение координат тайла на предыдущем уровне
-				parentTileX = offsetParentX / 256;
-				parentTileY = offsetParentY / 256;
+                    // необходимо возвращать, во сколько раз увеличить!!!
+                    bitmap = localProvider.get(new RawTile(parentTileX, parentTileY, tmpZ));
+                    if (bitmap == null) {
+                    } else { // родительский тайл найден и загружен
+                            // получение отступа в родительском тайле
+                            offsetParentX = offsetParentX - parentTileX * 256;
+                            offsetParentY = offsetParentY - parentTileY * 256;
 
-				offsetParentX = offsetParentX - parentTileX * 256;
-				offsetParentY = offsetParentY - parentTileY * 256;
+                            // получение уровня скалирования
+                            int scale = tmpZ - z;
+                            // получение размера тайла в родительском тайле
+                            int tileSize = getTileSize(scale);
 
-				// необходимо возвращать, во сколько раз увеличить!!!
+                            // копирование области и скалирование
+                            int[] pixels = new int[tileSize * tileSize];
+                            if (offsetParentY >= 0 && offsetParentX >= 0) {
+                                    bitmap.getPixels(pixels, 0, tileSize, offsetParentX,
+                                                    offsetParentY, tileSize, tileSize);
+                                    bitmap = Bitmap.createBitmap(pixels, tileSize,
+                                                    tileSize, Config.RGB_565);
+                                    pixels = null;
+                                    return Bitmap.createScaledBitmap(bitmap, 256, 256,
+                                                    false);
+                            }
+                    }
+            }
+            return bitmap;
+    }
 
-				bitmap = localProvider.get(new RawTile(parentTileX,
-						parentTileY, tmpZ));
-				if (bitmap == null) {
-				} else { // родительский тайл найден и загружен
-					// получение отступа в родительском тайле
-					offsetParentX = offsetParentX - parentTileX * 256;
-					offsetParentY = offsetParentY - parentTileY * 256;
+}
 
-					// получение уровня скалирования
-					// получение размера тайла в родительском тайле
-					int tileSize = getTileSize(scale);
+		
 
-					// копирование области и скалирование
-					int[] pixels = new int[tileSize * tileSize];
-					if (offsetParentY >= 0 && offsetParentX >= 0) {
-						bitmap.getPixels(pixels, 0, tileSize, offsetParentX,
-								offsetParentY, tileSize, tileSize);
-						bitmap = Bitmap.createBitmap(pixels, tileSize,
-								tileSize, Config.RGB_565);
-						pixels = null;
-						return Bitmap.createScaledBitmap(bitmap, 256, 256,
-								false);
-					}
-				}
-			}
-			return bitmap;
-		}
-
-	}
 
 }
