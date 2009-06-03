2b428c06c716e6e4ee9d4d34742371274e8ca9c6
hudvin <hudvin@70bebeea-5060-11de-81c9-c396dc251947>
2009-06-03 18:30:28 +0000

    добавлена проверка параметров тайла на допустимость зум рабоет хреново

--- src/com/nevilon/moow/MoowMap.java
+++ src/com/nevilon/moow/MoowMap.java
@@ -86,6 +86,8 @@ public class MoowMap extends Activity {
 
 		return super.onTouchEvent(event);
 	}
+	
+	
 
 	/**
 	 * Устанавливает состояние zoomIn/zoomOut контролов в зависимости от уровня
@@ -127,8 +129,6 @@ public class MoowMap extends Activity {
 		tdx = dx;
 		tdy = dy;
 		
-		//pmap.move(dx, dy);
-
 		
 		if (pmap.globalOffset.x > 0) {
 			dx = Math.round((pmap.globalOffset.x + 320) / 256);
@@ -234,6 +234,7 @@ public class MoowMap extends Activity {
 		}
 
 	}
+	
 
 	class ZoomPanel extends RelativeLayout {
 
