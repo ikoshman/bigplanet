956b704cc40f191ab5e6aefd966f17a95558c8d9
hudvin <hudvin@70bebeea-5060-11de-81c9-c396dc251947>
2009-06-03 18:30:28 +0000

    добавлена проверка параметров тайла на допустимость зум рабоет хреново

--- trunk/src/com/nevilon/moow/core/PhysicMap.java
+++ trunk/src/com/nevilon/moow/core/PhysicMap.java
@@ -14,11 +14,10 @@ public class PhysicMap {
 	private Bitmap[][] cells = new Bitmap[3][3];
 
 	private RawTile defTile;
-	
-	private int zoom;
-	
-	public Point globalOffset = new Point();
 
+	private int zoom;
+
+	public Point globalOffset = new Point();
 
 	public PhysicMap(RawTile defTile) {
 		this.defTile = defTile;
@@ -30,20 +29,21 @@ public class PhysicMap {
 	public RawTile getDefaultTile() {
 		return this.defTile;
 	}
-	
-	public int getZoomLevel(){
+
+	public int getZoomLevel() {
 		return this.zoom;
 	}
 
 	/**
-	 * Callback method 
+	 * Callback method
+	 * 
 	 * @param bitmap
 	 * @param tile
 	 */
-	public synchronized  void update(Bitmap bitmap, RawTile tile) {
+	public synchronized void update(Bitmap bitmap, RawTile tile) {
 		int dx = tile.x - defTile.x;
 		int dy = tile.y - defTile.y;
-		if (dx <= 2 && dy <= 2 && tile.z==defTile.z) {
+		if (dx <= 2 && dy <= 2 && tile.z == defTile.z) {
 			if (dx >= 0 && dy >= 0) {
 				try {
 					cells[dx][dy] = bitmap;
@@ -55,81 +55,108 @@ public class PhysicMap {
 
 	}
 
-	public void move(int dx, int dy){	
-		reload(defTile.x -dx,defTile.y - dy, defTile.z);
-		
+	public void move(int dx, int dy) {
+		reload(defTile.x - dx, defTile.y - dy, defTile.z);
+
 	}
-	
+
 	public Bitmap[][] getCells() {
 		return cells;
 	}
 
 	public void zoom(int x, int y, int z) {
-		reload(x-1,y-1,z);
+		reload(x, y, z);
 	}
 
-	
 	/**
 	 * Уменьшение уровня детализации
 	 */
-	public void zoomOut(){
-		if((zoom)<16){
-			int currentZoomX = getDefaultTile().x*256-globalOffset.x+160;
-			int currentZoomY = getDefaultTile().y*256-globalOffset.y+240;
-			int tileX = (currentZoomX/2)/256;
-			int tileY = (currentZoomY/2)/256;
+	public void zoomOut() {
+		if ((zoom) < 16) {
+			int currentZoomX = getDefaultTile().x * 256 - globalOffset.x + 160;
+			int currentZoomY = getDefaultTile().y * 256 - globalOffset.y + 240;
+			int tileX = (currentZoomX / 2) / 256;
+			int tileY = (currentZoomY / 2) / 256;
 			zoom++;
 			zoom(tileX, tileY, zoom);
 
 		}
-		
+
 	}
-	
+
 	/**
 	 * Увеличение уровня детализации с центрированием
 	 */
-	public void zoomInCenter(){
+	public void zoomInCenter() {
 		zoomIn(160, 240);
 	}
-	
+
 	/**
 	 * Увеличение уровня детализации
+	 * 
 	 * @param offsetX
 	 * @param offsetY
 	 */
-   public void zoomIn(int offsetX, int offsetY){
-		if(zoom>0){
-			//получение отступа он начала координат
-			int currentZoomX = getDefaultTile().x*256-globalOffset.x+offsetX;
-			int currentZoomY = getDefaultTile().y*256-globalOffset.y+offsetY;
-			// получение координат углового тайла
-			int tileX = (currentZoomX*2)/256;
-			int tileY = (currentZoomY*2)/256;
+	public void zoomIn(int offsetX, int offsetY) {
+		if (zoom > 0) {
+
+			// выравнивание ДОЛЖНО быть ПО ЦЕНТРУ
+
+			// получение отступа он начала координат
+			int currentZoomX = getDefaultTile().x * 256 - globalOffset.x
+					+ offsetX;
+			int currentZoomY = getDefaultTile().y * 256 - globalOffset.y
+					+ offsetY;
+
+			// получение координат точки на новом уровне
+			int nextZoomX = currentZoomX * 2;
+			int nextZoomY = currentZoomY * 2;
+
+			// получение координат угла экрана на новом уровне
+			nextZoomX = nextZoomX - 320 / 2;
+			nextZoomY = nextZoomY - 480 / 2;
+
+			// получение углового тайла
+			int tileX = nextZoomX / 256;
+			int tileY = nextZoomY / 256;
+
+			// отступ всегда один - точка должна находится в центре экрана
+			int correctionX = nextZoomX - tileX * 256;
+			int correctionY = nextZoomY - tileY * 256;
+
+			globalOffset.x = 320 / 2 - (correctionX);
+			globalOffset.y = 480 / 2 - (correctionY);
 			zoom--;
 			zoom(tileX, tileY, zoom);
 		}
 	}
 
-	
-	private void reload(int x, int y, int z){
-		int tileCount = (int) Math.pow(2, 17-defTile.z);
-		/*if(y<0){return;}
-		if(x<0){
-			x = tileCount+x;
-			System.out.println("offsetX " +globalOffset.x);
-			globalOffset.x = 320+globalOffset.x-256;
-		} else if(x>tileCount-1){
-			x = Math.abs(x-tileCount);
-			globalOffset.x = globalOffset.x -256;
-		}
-		*/
+	private void reload(int x, int y, int z) {
+		int tileCount = (int) Math.pow(2, 17 - defTile.z);
+		/*
+		 * if(y<0){return;} if(x<0){ x = tileCount+x;
+		 * System.out.println("offsetX " +globalOffset.x); globalOffset.x =
+		 * 320+globalOffset.x-256; } else if(x>tileCount-1){ x =
+		 * Math.abs(x-tileCount); globalOffset.x = globalOffset.x -256; }
+		 */
 		defTile.x = x;
 		defTile.y = y;
 		defTile.z = z;
 		loadCells(defTile);
 	}
-	
-	
+
+	private boolean checkTileXY(int x, int y, int z) {
+		if (x < 0 || y < 0 || z < 0) {
+			return false;
+		}
+		int maxTile = (int) Math.pow(2, 17 - z) - 1;
+		if (x > maxTile || y > maxTile) {
+			return false;
+		}
+		return true;
+
+	}
+
 	/**
 	 * Запрос на загрузку тайлов для данной группы ячеек (определяется по
 	 * крайней левой верхней)
@@ -144,24 +171,26 @@ public class PhysicMap {
 				x = (tile.x + i);
 				y = (tile.y + j);
 
-				/*int tileCount = (int) Math.pow(2, 17-defTile.z);
-				
-				if(x<0){
-					x = tileCount+x;
-				} else if(x>tileCount-1){
-					x = Math.abs(tileCount-x);
-				}
-				*/
-				tmpBitmap = tileProvider.inMemoryCache.get(new RawTile(x,y,tile.z));
-				if(tmpBitmap!=null){
-					cells[i][j] = tmpBitmap;
-				} else {
+				/*
+				 * int tileCount = (int) Math.pow(2, 17-defTile.z);
+				 * 
+				 * if(x<0){ x = tileCount+x; } else if(x>tileCount-1){ x =
+				 * Math.abs(tileCount-x); }
+				 */
+				if (!checkTileXY(x, y, tile.z)) {
 					cells[i][j] = null;
-					tileProvider.getTile(new RawTile(x, y, tile.z));
+				} else {
+					tmpBitmap = tileProvider.inMemoryCache.get(new RawTile(x,y,tile.z));
+					if(tmpBitmap!=null){
+						cells[i][j] = tmpBitmap;
+					} else {
+						cells[i][j] = null;
+						tileProvider.getTile(new RawTile(x, y, tile.z));
+					}
 				}
+
 			}
 		}
 	}
-	
 
 }
