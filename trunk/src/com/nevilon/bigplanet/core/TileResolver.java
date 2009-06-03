0086b024874bdbceab38013e1528297b7b25619a
hudvin <hudvin@70bebeea-5060-11de-81c9-c396dc251947>
2009-06-03 19:45:34 +0000

    увеличение производительности отрисовки

--- trunk/src/com/nevilon/bigplanet/core/TileResolver.java
+++ trunk/src/com/nevilon/bigplanet/core/TileResolver.java
@@ -14,7 +14,7 @@ public class TileResolver {
 
 	private PhysicMap physicMap;
 
-	private BitmapCacheWrapper cacheProvider = new BitmapCacheWrapper();
+	private BitmapCacheWrapper cacheProvider = BitmapCacheWrapper.getInstance();
 
 	private Handler scaledHandler;
 
@@ -22,6 +22,7 @@ public class TileResolver {
 
 	private int strategyId = -1;
 
+	
 	public TileResolver(final PhysicMap physicMap) {
 		this.physicMap = physicMap;
 		tileLoader = new TileLoader(
@@ -32,13 +33,11 @@ public class TileResolver {
 						if (tile.s == strategyId) {
 							LocalStorageWrapper.put(tile, data);
 							Bitmap bmp = LocalStorageWrapper.get(tile);
-						//	cacheProvider.putToCache(tile, bmp);
+							cacheProvider.putToCache(tile, bmp);
 							updateMap(tile, bmp);
 						}
 					}
 				});
-		//TODO может можно убрать
-		//setMapSource(strategyId);
 		new Thread(tileLoader).start();
 
 		// обработчик загрузки скалированых картинок
@@ -53,8 +52,7 @@ public class TileResolver {
 						cacheProvider.putToScaledCache(tile, bitmap);
 					}
 				}
-
-			}
+						}
 
 		};
 		// обработчик загрузки с дискового кеша
@@ -63,20 +61,19 @@ public class TileResolver {
 			@Override
 			public synchronized void handle(RawTile tile, Bitmap bitmap,
 					boolean isScaled) {
-				
-				if (tile.s != strategyId) {
+				if (tile.s != strategyId) { // если не текущий провайдер
 					return;
-				}
-				if (bitmap != null) {
+				} 
+				if (bitmap != null) { // если тайл есть в файловом кеше
 					updateMap(tile, bitmap);
-				//	cacheProvider.putToCache(tile, bitmap);
-				} else {
-
+					cacheProvider.putToCache(tile, bitmap);
+				} else {  // если тайла нет в файловом кеше
+					
 					bitmap = cacheProvider.getScaledTile(tile);
 					if (bitmap == null) {
 						new Thread(new TileScaler(tile, scaledHandler)).start();
 
-					} else {
+					} else { // скалированый тайл из кеша
 						updateMap(tile, bitmap);
 
 					}
@@ -89,7 +86,7 @@ public class TileResolver {
 	}
 
 	private void load(RawTile tile) {
-		if(tile.s!=-1){
+		if (tile.s != -1) {
 			tileLoader.load(tile);
 		}
 	}
@@ -107,13 +104,17 @@ public class TileResolver {
 	public void getTile(final RawTile tile, boolean useCache) {
 		Bitmap bitmap = null;
 		if (useCache) {
-		//	bitmap = cacheProvider.getTile(tile);
+			bitmap = cacheProvider.getTile(tile);
 			if (bitmap != null) {
+				// возврат тайла
 				updateMap(tile, bitmap);
 				return;
+			} else {
+				LocalStorageWrapper.get(tile, localLoaderHandler);
 			}
+		} else {
+			LocalStorageWrapper.get(tile, localLoaderHandler);
 		}
-		LocalStorageWrapper.get(tile, localLoaderHandler);
 	}
 
 	public synchronized void setMapSource(int sourceId) {
--- trunk/src/com/nevilon/bigplanet/core/storage/BitmapCacheWrapper.java
+++ trunk/src/com/nevilon/bigplanet/core/storage/BitmapCacheWrapper.java
@@ -15,11 +15,24 @@ public class BitmapCacheWrapper {
 	/*
 	 * размер кеша (для каждого обоих типов)
 	 */
-	public final static int CACHE_SIZE = 10;
+	public final static int CACHE_SIZE = 20;
 	private BitmapCache cache = new BitmapCache(CACHE_SIZE);
 
 	private BitmapCache scaledCache = new BitmapCache(CACHE_SIZE);
 
+	private static BitmapCacheWrapper instance;
+	
+	public static BitmapCacheWrapper getInstance(){
+		if(instance == null){
+			instance = new BitmapCacheWrapper();
+		}
+		return instance;
+	}
+	
+	private BitmapCacheWrapper(){
+		
+	}
+	
 	/**
 	 * Поиск в кеше скалированых тайлов
 	 * 
@@ -43,16 +56,16 @@ public class BitmapCacheWrapper {
 	 * @param tile
 	 * @return
 	 */
-//	public Bitmap getTile(RawTile tile) {
-//		return cache.get(tile);
-//	}
+	public Bitmap getTile(RawTile tile) {
+		return cache.get(tile);
+	}
 
 	/**
 	 * Помещает битмап в кеш тайлов
 	 */
-	//public void putToCache(RawTile tile, Bitmap bitmap) {
-	//	cache.put(tile, bitmap);
-//	}/
+	public void putToCache(RawTile tile, Bitmap bitmap) {
+	cache.put(tile, bitmap);
+	}
 
 	public void clear() {
 		scaledCache.clear();
