886d4fec0d60e7cf79391da963cdccedb6a1eeb1
hudvin <hudvin@70bebeea-5060-11de-81c9-c396dc251947>
2009-06-03 18:49:22 +0000

    частично исправлен баг с миганеим, рефакторинг

---
+++ trunk/src/com/nevilon/moow/core/ui/BitmapUtils.java
@@ -0,0 +1,47 @@
+package com.nevilon.moow.core.ui;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Bitmap.Config;
+
+/**
+ * Утилиты для работы с битмапами
+ * @author hudvin
+ *
+ */
+public class BitmapUtils {
+
+	/**
+	 * Рисует фон для карты( в клетку )
+	 * @param cellSize
+	 * @param height
+	 * @param widht
+	 * @return
+	 */
+	public static Bitmap drawBackground(int cellSize, int height, int widht) {
+		// создание битмапа по размеру экрана
+		Bitmap bitmap = Bitmap.createBitmap(320, height, Config.RGB_565);
+		Canvas cv = new Canvas(bitmap);
+		// прорисовка фона
+		Paint background = new Paint();
+		background.setARGB(255, 128, 128, 128);
+		cv.drawRect(0, 0, 320, height, background);
+		background.setAntiAlias(true);
+		// установка цвета линий
+		background.setColor(Color.WHITE);
+		// продольные линии
+		for (int i = 0; i < 320 / cellSize; i++) {
+			cv.drawLine(cellSize * i, 0,
+					cellSize * i, height, background);
+		}
+		// поперечные линии
+		for (int i = 0; i < height / cellSize; i++) {
+			cv.drawLine(0, cellSize * i, 320,
+					cellSize * i, background);
+		}
+		return bitmap;
+	}
+	
+}
