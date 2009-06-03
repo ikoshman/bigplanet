2a8eb5f6914183d153649b61fb6d3889d3678928
hudvin <hudvin@70bebeea-5060-11de-81c9-c396dc251947>
2009-06-03 18:21:42 +0000

    добавлен код для скалирования доступных тайлов пока закомментирован

--- src/com/nevilon/moow/core/TileProvider.java
+++ src/com/nevilon/moow/core/TileProvider.java
@@ -5,6 +5,8 @@ import java.util.Stack;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.graphics.Bitmap.Config;
 import android.util.Log;
 
 public class TileProvider implements Runnable {
@@ -47,6 +49,49 @@ public class TileProvider implements Runnable {
 					inMemoryCache.put(tile, tmpBitmap);
 					returnTile(tmpBitmap, tile);
 				} else {
+					
+					/*
+					double scaledX = tile.getX()/2.0;
+				 	double scaledY = tile.getY()/2.0;
+				 	int tx = (int) Math.floor(scaledX);
+				 	int ty = (int) Math.floor(scaledY);
+				 	int ox, oy;
+				 	if (tx<scaledX){
+				 		ox = 1;
+				 	} else {
+				 		ox = 0;
+				 	}
+				 	if(ty<scaledY){
+				 		oy = 1;
+				 	} else {
+				 		oy = 0;
+				 	}
+				 	Bitmap bmp4scale;
+				 	RawTile tile4scale = new RawTile(tx, ty,tile.getZ()+1);
+				 	bmp4scale = inMemoryCache.get(tile);
+				 	if(bmp4scale == null){
+				 		outStream = localStorage.get(tile4scale); 
+				 		if (outStream!=null){
+				 			bmp4scale = BitmapFactory.decodeStream(outStream);
+				 		}
+				 	}
+				 	if (bmp4scale!=null){
+				 		Bitmap scaledBitmap = Bitmap.createBitmap(128, 128, Config.ARGB_8888);
+				 		for(int i = 0;i<128;i++){
+				 			for(int j = 0; j<128; j++){
+				 				scaledBitmap.setPixel(i, j, bmp4scale.getPixel(i+ox, j+oy));
+				 			}
+				 		}
+				 		
+				 		
+				 		scaledBitmap = Bitmap.createScaledBitmap(scaledBitmap, 256, 256, false);
+				 		
+				 	 	returnTile(scaledBitmap, tile);
+				 	
+				 	}
+				 	*/
+					
+					// return scaled tile
 					tileLoader.load(tile);
 				}
 
